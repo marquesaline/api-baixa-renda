ee01e8fccd31c9b42707bd291e0d0c7d00678bd8
marquesaline <alinem_oliveira@yahoo.com>
2022-12-20 09:57:22 -0300

    configuração do jest para testes

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,9 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World!');
+    res
+        .status(200)
+        .json('Hello World!');
 })
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import App from './app';
 
+const PORT = process.env.PORT || 3000;
+
 const server = new App();
-server.start();
+
+server.app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
 
 export default server;
